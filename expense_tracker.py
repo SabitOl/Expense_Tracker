1e7d0c893bec6ecbef064d073c358949d6f22042
SabitOl <cuttingdew@gmail.com>
2026-06-28 13:28:10 +0100

    Add expense_tracker.py

---
+++ expense_tracker.py
@@ -0,0 +1,8 @@
+amount = int(input("How much did you spend? "))
+category = input("On what did you spend the money? For instance food, rent, etc. ")
+description = input("Enter description for your spending: ")
+date = input("Enter the date of your spending: ")
+
+print(f"Amount: {amount}, Category: {category}, Description: {description}, Date: {date}")
+# Add and save them to a csv file
+
