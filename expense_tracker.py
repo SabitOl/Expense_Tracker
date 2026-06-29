854fa3f67f50c9c29277ed0f4866544ebffc0f18
SabitOl <cuttingdew@gmail.com>
2026-06-29 18:26:29 +0100

    Include error handling

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,26 +1,41 @@
 import csv
+import re
+import sys
 
-amount = int(input("How much did you spend? "))
-category = input("On what did you spend the money? For instance food, rent, etc. ")
-description = input("Enter description for your spending: ")
-date = input("Enter the date of your spending: ")
+print("Welcome to the Expense tracker!")
 
-with open("storage.csv", "a", newline="") as file:
-    actions = csv.DictWriter(file, fieldnames=["amount", "category", "description", "date"])
-    actions.writerow({"amount": amount, "category": category, "description": description, "date": date})
+try:
+    for i in range(4):
+        amount = int(input("How much did you spend? "))
+        category = (input("What did you spend the money on? For instance food, rent, etc. ")).strip()
+        if not category.isalpha():
+            print("Invalid category! The category should be a string/word")
+            sys.exit()
+        description = input("Enter description for your spending: ")
+        date = input("Enter the date of your spending: ")
+        if re.search(r"[a-zA-Z]", date):
+            print("Invalid date! The date should be in numbers and symbols (eg. -, /) allowed")
+            sys.exit()
+        
+        with open("storage.csv", "a", newline="") as file:
+            actions = csv.DictWriter(file, fieldnames=["amount", "category", "description", "date"])
+            actions.writerow({"amount": amount, "category": category, "description": description, "date": date})
+                  
+except ValueError:
+    print(f"invalid input!")
+    sys.exit()
 
 def main():
-    my_total = total_amount()
-    print(f"The total sum of money you spent is ${my_total}")
-
-def total_amount():
     total = 0
     with open("storage.csv", "r") as file:
         reader = csv.DictReader(file)
         for amt in reader:
             amounts = int(amt["amount"])
             total += amounts
-        return total
+        print(f"The total sum of money you spent is ${total}")
+
+if __name__ == "__main__":
+    main()
+
      
-main()
 
