5d3e1acf6611b73a3bf1f37ba2e5d97d50f4e046
SabitOl <cuttingdew@gmail.com>
2026-06-30 21:21:19 +0100

    Modife exchange_tracker.py and add challenges.md

---
+++ challenges.md
@@ -0,0 +1,9 @@
+# Here's what I encountered building this and some decisions I made
+
+- Why have two while loops? It's to allow for restarting and continuity ofuser input. 
+
+When the inner loops breaks as a result of wrong value entered, the inner loops only breaks. And then returns to the outer loop. This restarts the expense tracker automatically.
+
+- I noticed without "break" and a user enters the wrong value type, it still gets added to the storage csv file.
+
+So, I decided to include "break" at some points. This stops wrong value to be ever written in the csv file.
--- expense_tracker.py
+++ expense_tracker.py
@@ -4,35 +4,38 @@ import sys
 
 print("Welcome to the Expense tracker!")
 
-try:
-    for i in range(4):
-        amount = int(input("How much did you spend? "))
-        category = (input("What did you spend the money on? For instance food, rent, etc. ")).strip()
-        if not category.isalpha():
-            print("Invalid category! The category should be a string/word")
-            sys.exit()
-        description = input("Enter description for your spending: ")
-        date = input("Enter the date of your spending: ")
-        if re.search(r"[a-zA-Z]", date):
-            print("Invalid date! The date should be in numbers and symbols (eg. -, /) allowed")
-            sys.exit()
-        
-        with open("storage.csv", "a", newline="") as file:
-            actions = csv.DictWriter(file, fieldnames=["amount", "category", "description", "date"])
-            actions.writerow({"amount": amount, "category": category, "description": description, "date": date})
+while True:
+    try:
+        while True:
+            amount = int(input("How much did you spend? "))
+            category = (input("What did you spend the money on? For instance food, rent, etc. ")).strip()
+            if re.search(r"[0-9]", category):
+                print("Invalid category! The category should be a string/word")
+                break
+            description = input("Enter description for your spending: ")
+            date = input("Enter the date of your spending: ")
+            if re.search(r"[a-zA-Z]", date):
+                print("Invalid date! The date should be in numbers and symbols (eg. -, /) allowed")
+                break
+            
+            with open("storage.csv", "a", newline="") as file:
+                actions = csv.DictWriter(file, fieldnames=["amount", "category", "description", "date"])
+                actions.writerow({"amount": amount, "category": category, "description": description, "date": date})
+            
+            print("Write 'exit'")
                   
-except ValueError:
-    print(f"invalid input!")
-    sys.exit()
+    except ValueError:
+        print(f"invalid input!")
 
-def main():
-    total = 0
-    with open("storage.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for amt in reader:
-            amounts = int(amt["amount"])
-            total += amounts
-        print(f"The total sum of money you spent is ${total}")
+    def main():
+        total = 0
+        with open("storage.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for amt in reader:
+                amounts = int(amt["amount"])
+                total += amounts
+            print(f"The total sum of money you spent is ${total}")
+
 
 if __name__ == "__main__":
     main()
